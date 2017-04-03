f1bb3b929794ac7023120a0c866a249b15d73de3
bufan <bufan@192.168.1.118>
2017-04-03 17:45:57 +0800

    add

---
+++ 流程图/第一课.py
@@ -0,0 +1,17 @@
+'''
+Created on 2017年4月3日
+
+@author: bufan
+'''
+num_1=int(input("请输入第一个数字:"))
+num_2=int(input("请输入第二个数字:"))
+
+if num_1 == 0  and num_2 == 0:
+    print("两个数字都是0")
+    exit
+    
+sum_num = num_1+num_2
+if sum_num % 2 == 0:
+    print("他们的和是偶数")
+else:
+    print("他们的和是奇数")   
