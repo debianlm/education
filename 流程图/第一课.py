745cff490e88b70fafeca092079c1d2e92aecd02
bufan <bufan@192.168.1.118>
2017-04-03 17:47:19 +0800

    use else and instead of default case

--- 流程图/第一课.py
+++ 流程图/第一课.py
@@ -8,10 +8,9 @@ num_2=int(input("请输入第二个数字:"))
 
 if num_1 == 0  and num_2 == 0:
     print("两个数字都是0")
-    exit
-    
-sum_num = num_1+num_2
-if sum_num % 2 == 0:
-    print("他们的和是偶数")
 else:
-    print("他们的和是奇数")   
+    sum_num = num_1+num_2
+    if sum_num % 2 == 0:
+        print("他们的和是偶数")
+    else:
+        print("他们的和是奇数")   
